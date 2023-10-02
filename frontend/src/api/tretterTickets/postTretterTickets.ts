fa827c65a4381db161cb8065a9dcf532bb031d7e
Thubasa-Sato <thubasa.sato.engineer@gmail.com>
2023-10-03 00:39:52 +0900

    エラーハンドリングのためにコード修正 #3

--- frontend/src/api/tretterTickets/postTretterTickets.ts
+++ frontend/src/api/tretterTickets/postTretterTickets.ts
@@ -1,11 +1,20 @@
-import { useState, useEffect } from 'react';
-import { ApiError, handleApiError, handleApiResponse, httpClient, IHandleApiResponseRet } from '../axiosHandlers';
-import { POST_TICKET_PATH } from './path';
+import { useState, useEffect } from "react";
+import {
+  ApiError,
+  handleApiError,
+  handleApiResponse,
+  httpClient,
+  IHandleApiResponseRet,
+} from "../axiosHandlers";
+import { POST_TICKET_PATH } from "./path";
 
-type Resp = '';
-
-type Ret = IHandleApiResponseRet<Resp> | ApiError;
+type Resp = "";
 
 export const postTretterTickets = async () => {
-  await httpClient.post(POST_TICKET_PATH);
+  try {
+    const response = await httpClient.post(POST_TICKET_PATH);
+    return handleApiResponse<Resp>({ response, expectedStatus: 204 });
+  } catch (error: unknown) {
+    return handleApiError({ error, methodName: postTretterTickets.name });
+  }
 };
