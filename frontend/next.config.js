3685ad1c5ed5747e0bdb04fb15a1a0ffc4c6edf9
Thubasa-Sato <thubasa.sato.engineer@gmail.com>
2023-10-31 17:42:42 +0900

    環境変数の追加、packageをyarnで再追加、管理

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const {
+  APP_NAME,
+  API_BASE_URL,
+  NEED_DEBUG,
+ } = process.env;
+
+module.exports = {
+  env: {
+    APP_NAME,
+    API_BASE_URL,
+    NEED_DEBUG,
+  }
+}
