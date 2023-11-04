f57b3b14a267867c1a22c8a831e10decf379e19a
tubasaHagiwara <thubasa.sato.engineer@gmail.com>
2023-11-04 12:41:40 +0900

    不具合解消により実装予定だった部分のコメント解除

--- frontend/src/api/hooks/currentUser/useFetchCurrentUser.ts
+++ frontend/src/api/hooks/currentUser/useFetchCurrentUser.ts
@@ -1,72 +1,55 @@
-// import type { CurrentUserResp } from "src/api/schemas/user";
+import type { UserResp } from "src/api/schemas/user";
 
-// import { useEffect, useState } from "react";
-// import { useFetcher } from "src/api/hooks/useFetcher";
-// import { useAppDispatch } from "src/store/hooks";
-// import { currentUserRespSchema } from "src/api/schemas/user";
-
-// export const useFetchCurrentUser = () => {
-//   const { data, error, mutate, inProgress, skipped } = useFetcher<undefined,CurrentUserResp>({
-//     req: { path: "/current_user" },
-//     resp: {
-//       expectedStatus: 200,
-//       bodyValidator: (respBody) => currentUserRespSchema.parse(respBody),
-//     },
-//     // callbacks: {
-//     //   beforeReq: () => {
-//     //     // startLoadings();
-//     //   },
-//     //   afterReq: () => {
-//     //     // stopLoadings();
-//     //   },
-//     // },
-//     methodName: useFetchCurrentUser.name,
-//   });
-
-//   const [currentUser, setCurrentUser] = useState<CurrentUserResp>();
-
-//   const dispatch = useAppDispatch();
-
-//   useEffect(() => {
-//     if (inProgress || skipped) return;
-
-//     if (data?.isSuccess && !error) {
-//       // 取得に成功したらcurrentUserをstoreにセット
-//       // dispatch(setCurrentUserAttrs(data.responseData.data.attributes));
-//       // dispatch(setLoginLabels(DEFAULT_LOGIN_LABELS));
-//       console.log("成功");
-//       console.log("currentUser", currentUser);
-//       setCurrentUser(data.responseData);
-//     } else {
-//       // 取得に失敗したら、currentUserをクリアして、再認証用のログインフォーム表示準備
-//       // dispatch(resetCurrentUser());
-//       // dispatch(setLoginLabels(LOGIN_LABELS_RE_LOGIN));
-//       console.log("失敗");
-//       console.log("currentUser", currentUser);
-//       setCurrentUser(undefined);
-//     }
-//   }, [inProgress, skipped, data, error, dispatch]);
-
-//   return { currentUser, error, mutate };
-// };
-
-import useSWR from 'swr';
-
-import { fetchCurrentUser } from '../../currentUser';
-
-import type { ApiError, IHandleApiResponseRet } from '../../axiosHandlers';
-import type { UserResp } from '../../schemas/user';
+import { useEffect, useState } from "react";
+import { useFetcher } from "src/api/hooks/useFetcher";
+import { useAppDispatch } from "src/store/hooks";
+import {
+  setAttributes as setCurrentUserAttrs,
+  resetState as resetCurrentUser,
+} from 'src/store/slices/currentUserSlice';
+import { userRespSchema } from "src/api/schemas/user";
+import { useLoadings } from 'src/hooks/useLoadings';
 
 export const useFetchCurrentUser = () => {
-  const { path, fetcher } = fetchCurrentUser;
-
-  const { data, error, mutate } = useSWR<IHandleApiResponseRet<UserResp>, ApiError>(path, fetcher, {
-    onErrorRetry: (error, _key, _config, _revalidate, { retryCount }) => {
-      if (error.status === 401) return;
-
-      if (retryCount > 5) return;
+  const { startLoadings, stopLoadings } = useLoadings({ triggers: ['fetchCurrentUser'] });
+  // , inProgress, skipped
+  const { data, error, mutate } = useFetcher<undefined,UserResp>({
+    req: { path: "/current_user" },
+    resp: {
+      expectedStatus: 200,
+      bodyValidator: (respBody) => userRespSchema.parse(respBody),
     },
+    callbacks: {
+      beforeReq: () => {
+        startLoadings();
+      },
+      afterReq: () => {
+        stopLoadings();
+      },
+    },
+    methodName: useFetchCurrentUser.name,
   });
 
-  return { data, error, mutate };
+  const [currentUser, setCurrentUser] = useState<UserResp>();
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    // if (inProgress || skipped) return;
+
+    if (data?.isSuccess && !error) {
+      // 取得に成功したらcurrentUserをstoreにセット
+      dispatch(setCurrentUserAttrs(data.responseData.data.attributes));
+      // dispatch(setLoginLabels(DEFAULT_LOGIN_LABELS));
+      setCurrentUser(data.responseData);
+    } else {
+      // 取得に失敗したら、currentUserをクリアして、再認証用のログインフォーム表示準備
+      dispatch(resetCurrentUser());
+      // dispatch(setLoginLabels(LOGIN_LABELS_RE_LOGIN));
+      setCurrentUser(undefined);
+    }
+  }, [data, error, dispatch]);
+  // inProgress, skipped,
+
+  return { currentUser, error, mutate };
 };
